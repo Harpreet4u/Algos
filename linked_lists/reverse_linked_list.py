8fba3114d862b368c947308700c258dc70a94418
harpreet4u <happysinghsingh7@gmail.com>
2017-07-24 15:23:48 +0530

    reverse a linked list recursive algo

---
+++ linked_lists/reverse_linked_list.py
@@ -0,0 +1,13 @@
+# Recursive algo for reversing linked list.
+
+
+def reverse_list(head):
+
+    if head->next == None:
+        return head
+
+    newHead = reverse_list(head->next)
+    head->next->next = head
+    head->next = None
+    return newHead
+
